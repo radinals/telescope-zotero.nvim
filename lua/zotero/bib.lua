72a7c3d3e3cdb9eff72aef375cb6e632680aab67
radinals <radinalsaragih@gmail.com>
2025-10-21 06:07:47 +0700

    pattern match for the syntax instead

--- lua/zotero/bib.lua
+++ lua/zotero/bib.lua
@@ -139,24 +139,11 @@ M.locate_org_bib = function()
 end
 
 local getLatexBibType = function()
-  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-  for _, line in ipairs(lines) do
-    -- ignore commented bibliography
-    local comment = string.match(line, '^%%')
-    if not comment then
-      -- checking for biblatex
-      local location = string.match(line, [[\addbibresource{[ "']*([^'"\{\}]+)["' ]*}]])
-      if location then
-        return "biblatex"
-      end
-      local location = string.match(line, [[\bibliography{[ "']*([^'"\{\}]+)["' ]*}]])
-      if location then
-        return "bibtex"
-      end
-
-      return "UNKNOWN"
-
-    end
+  vim.api.nvim_win_set_cursor(0, {1, 1})
+  if (vim.fn.search("\\V\\\\bibliography", "nw") ~= 0) then
+    return "bibtex"
+  elseif (vim.fn.search("\\V\\\\addbibresource", "nw") ~= 0 or vim.fn.search("\\V\\\\printbibliography", "nw") ~= 0 ) then
+    return "biblatex"
   end
 end
 
@@ -167,13 +154,22 @@ M.entry_to_bib_entry = function(entry)
 
   local itemType = item.itemType
 
+  vim.notify("Zotero returned ".. itemType .. "")
+
   if (getLatexBibType() == "biblatex") then
     itemType = biblatex(itemType)
+    vim.notify("detected biblatex format",vim.log.levels.INFO)
+  vim.notify("itemType translated to " .. itemType )
   elseif(getLatexBibType() == "bibtex") then
     itemType = bibtex(itemType)
+    vim.notify("detected bibtex format", vim.log.levels.INFO)
+  vim.notify("itemType translated to " .. itemType )
   elseif(vim.bo.filetype == "tex") then
     itemType = bibtex(itemType)
+    vim.notify("cannot detect bibliography format, fallback to bibtex", vim.log.levels.INFO)
+    vim.notify("itemType translated to " .. itemType )
   end
+
 
   bib_entry = bib_entry .. (itemType or " ") .. '{' .. citekey .. ',\n'
 
